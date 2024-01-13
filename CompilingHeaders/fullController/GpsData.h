9eb77d9eca0f68ef1c4a769cde45a7e9ecba2fd4
ch263107 <ch263107@falmouth.ac.uk>
2024-01-13 21:28:05 +0000

    header files for gps that compile

---
+++ CompilingHeaders/fullController/GpsData.h
@@ -0,0 +1,21 @@
+#ifndef GpsData_h
+#define GpsData_h
+
+#include <TinyGPS++.h>
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+
+class GpsClass
+{
+  public:
+    void Setup();
+    void GpsDataCollection();
+    SoftwareSerial GpsSerial();
+
+  private:
+  TinyGPSPlus gps;
+};
+
+extern GpsClass GpsData;
+
+#endif

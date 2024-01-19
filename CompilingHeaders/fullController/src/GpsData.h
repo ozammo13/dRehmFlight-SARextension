e996e3d80990967622bdbd66d1dd294cd23b2160
Tom Brothers <tb263251@falmouth.ac.uk>
2024-01-19 00:32:22 +0000

    Fixed error caused by missing libary and incorrectly innitiated software serial variable

--- CompilingHeaders/fullController/src/GpsData.h
+++ CompilingHeaders/fullController/src/GpsData.h
@@ -1,22 +1,30 @@
+/*
+GpsData.h - Library for easy GPS data.
+Created by Caleb Hamlet 2023
+Partially maintained by Tom Brothers 2023
+Released into the public domain.
+*/
+#include "Arduino.h"
 #ifndef GpsData_h
 #define GpsData_h
-#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 #include <TinyGPSPlus.h>
-#include "Arduino.h"
-#include "SoftwareSerial.h"
 
-class GpsClass
+class GpsData
 {
   public:
-    GpsClass();
-    void Setup();
+    GpsData(int RxPin, int TxPin);
+    void Setup(unsigned long SerialSpeed);
     void GpsDataCollection();
-    //SoftwareSerial GpsSerial();
-
-  private:
     TinyGPSPlus gps;
+    SoftwareSerial GpsSerial = SoftwareSerial(0,1);
+    //Variables
+    float Latt;
+    float Long;
+    float Alt;
+  private:
+    int RxPin;
+    int TxPin;
 };
-
-extern GpsClass GpsData;
-
+  
 #endif
